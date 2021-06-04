6b2ac88b035f13d867a1e32a955df4ba858e44ac
Deyvi-dev <68565306+Deyvi-dev@users.noreply.github.com>
2021-06-04 18:39:55 -0300

    Create portscan.py

---
+++ portscan.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import sys
+import socket
+
+ipAddress = sys.argv[1]
+
+for ports in range(1,65535):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    if s.connect_ex((ipAddress, ports)) == 0:
+        print(f"Porta {ports} Aberta!")
+        s.close()
